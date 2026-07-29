845352b8c98563240d84bb3d60db89753e5eddbc
MARWAN-PC\Marwan <marwanabdelwahab28@gmail.com>
2026-07-29 23:42:20 +0300

    finish splash feature

---
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hub/features/on_boarding/presentation/views/on_boarding_view.dart';
+import 'package:fruit_hub/features/splash/presentation/views/splash_view.dart';
+
+Route<dynamic> onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case SplashView.routeName:
+      return MaterialPageRoute(builder: (_) => const SplashView());
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (_) => const OnBoardingView());
+    default:
+      return MaterialPageRoute(
+        builder: (_) {
+          return Scaffold(
+            body: Center(child: Text('No route defined for ${settings.name}')),
+          );
+        },
+      );
+  }
+}
---
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fruit_hub/core/utils/app_images.dart';
+import 'package:fruit_hub/features/on_boarding/presentation/views/on_boarding_view.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    executeNavigation();
+  }
+
+  void executeNavigation() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(
+        context,
+        OnBoardingView.routeName,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      // crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [SvgPicture.asset(Assets.imagesPlant)],
+        ),
+        SvgPicture.asset(Assets.imagesLogo),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SvgPicture.asset(Assets.imagesSplashBottom, fit: BoxFit.fill),
+          ],
+        ),
+      ],
+    );
+  }
+}
