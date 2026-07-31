62d7fac8cc533fb3fd0d7330099313713be13b17
MARWAN-PC\Marwan <marwanabdelwahab28@gmail.com>
2026-07-31 18:29:23 +0300

    Add text styles

--- lib/features/on_boarding/presentation/widgets/on_boarding_page_view.dart
+++ lib/features/on_boarding/presentation/widgets/on_boarding_page_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/core/utils/app_colors.dart';
 import 'package:fruit_hub/core/utils/app_images.dart';
+import 'package:fruit_hub/core/utils/app_text_styles.dart';
 import 'package:fruit_hub/features/on_boarding/presentation/widgets/page_view_item.dart';
 
 class OnBoardingPageView extends StatelessWidget {
@@ -21,9 +23,13 @@ class OnBoardingPageView extends StatelessWidget {
           title: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text('مرحبًا بك في '),
-              const Text('HUB'),
-              const Text('Fruit'),
+               Text('مرحبًا بك في ',
+              style: TextStyles.bold23,
+              ),
+               Text('HUB',
+              style: TextStyles.bold23.copyWith(color: AppColors.secondaryColor),),
+               Text('Fruit',
+              style: TextStyles.bold23.copyWith(color: AppColors.primaryColor),),
             ],
           ),
         ),
--- lib/features/on_boarding/presentation/widgets/page_view_item.dart
+++ lib/features/on_boarding/presentation/widgets/page_view_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fruit_hub/core/utils/app_text_styles.dart';
 
 class PageViewItem extends StatelessWidget {
   const PageViewItem({
@@ -41,7 +42,10 @@ class PageViewItem extends StatelessWidget {
                 visible: isVisible,
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
-                  child: Text('تخط'),
+                  child: Text('تخط',
+                  style: TextStyles.regular13.copyWith(
+                    color: Color(0xFF949D9E),
+                  ),),
                 ),
               ),
             ],
@@ -51,8 +55,11 @@ class PageViewItem extends StatelessWidget {
         title,
         const SizedBox(height: 24),
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Text(subtitle, textAlign: TextAlign.center),
+          padding: const EdgeInsets.symmetric(horizontal: 37.0),
+          child: Text(subtitle, textAlign: TextAlign.center,
+          style: TextStyles.semiBold13.copyWith(
+            color: Color(0xFF4E5456),
+          ),),
         ),
       ],
     );
